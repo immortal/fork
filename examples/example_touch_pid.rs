b3229f8e5ec1b7d79e43f69f45ee8a56cbc432bd
nbari <nbari@tequila.io>
2024-07-21 18:40:21 +0200

    daemon examples

---
+++ examples/example_touch_pid.rs
@@ -0,0 +1,27 @@
+/// run with `cargo run --example example_touch_pid`
+use fork::{daemon, Fork};
+use std::fs::OpenOptions;
+use std::process::Command;
+
+fn main() {
+    match daemon(false, false) {
+        Ok(Fork::Child) => {
+            Command::new("sleep")
+                .arg("300")
+                .output()
+                .expect("failed to execute process");
+        }
+        Ok(Fork::Parent(pid)) => {
+            // touch file with name like pid
+            let file_name = format!("/tmp/{}.pid", pid);
+            OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(file_name)
+                .expect("failed to open file");
+        }
+        Err(_) => {
+            println!("Fork failed");
+        }
+    }
+}
